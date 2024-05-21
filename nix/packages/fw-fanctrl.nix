aee101197d52c5cde9cd834c1f809bc7a67ad6c5
Svenum <s.ziegler@holypenguin.net>
2024-05-21 17:29:31 +0200

    remove requiremetns.txt + add github actions

--- nix/packages/fw-fanctrl.nix
+++ nix/packages/fw-fanctrl.nix
@@ -24,9 +24,6 @@ python3Packages.buildPythonPackage rec{
     cat > setup.py << EOF
     from setuptools import setup
 
-    with open("requirements.txt") as f:
-        install_requires = f.read().splitlines()
-
     setup(
       name="fw-fanctrl",
       description="${description}",
@@ -34,7 +31,6 @@ python3Packages.buildPythonPackage rec{
       platforms=["linux"],
       py_modules=[],
 
-      install_requires=install_requires,
       scripts=[
         "fanctrl.py",
       ],
