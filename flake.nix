9ea8fb60a692d8482b3a10ac6a387124253f333b
Svenum <s.ziegler@holypenguin.net>
2024-06-01 16:49:57 +0200

    chagne flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A software to control the Framework fan speed";
+  description = "A simple systemd service to better control Framework Laptop's fan(s)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
