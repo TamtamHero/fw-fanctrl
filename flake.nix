8446f9d260a1502e0c9856787a336e8b341496b8
Svenum <43136984+Svenum@users.noreply.github.com>
2024-06-01 19:32:35 +0200

    Add NixOS Flake (#26)
    * initial
    
    * update gitignore
    
    * update inputs
    
    * add fw-ectool dependencie
    
    * add module
    
    * fix tabs
    
    * fix package
    
    * fix typo
    
    * fix service
    
    * fix type
    
    * add options
    
    * fix service
    
    * fix build inputs
    
    * add Readme + add suspend script
    
    * remove unneeded };
    
    * fix pkgs.writeShellScript
    
    * remvoe \
    
    * try
    
    * add self
    
    * fix module
    
    * update package
    
    * fix package
    
    * use sleep script
    
    * add config options
    
    * fix typo
    
    * fix typo
    
    * add defaults
    
    * fix type
    
    * add prettyier
    
    * remove beautifyer
    
    * udpate readme
    
    * update installer script
    
    * add missing path
    
    * Update README.md
    
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
    
    * Update flake.nix
    
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
    
    * Update nix/module.nix
    
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
    
    * add descriptions
    
    * fix uninstall
    
    * update readme
    
    * add description
    
    * remove requiremetns.txt + add github actions
    
    * update action
    
    * rename workflow test
    
    * fix service
    
    * try
    
    * try
    
    * Update README.md
    
    * Update README.md
    
    * chagne flake description
    
    * fix suspend script
    
    * fix script
    
    * fix path
    
    * fix install.sh
    
    * fix --no-sudo
    
    * add --no-sudo to other scripts
    
    * fix check
    
    * add option check
    
    * add missing "
    
    * Rename nix action
    
    ---------
    
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A simple systemd service to better control Framework Laptop's fan(s)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-compat }: {
+    packages.x86_64-linux.default = self.packages.x86_64-linux.fw-fanctrl;
+    packages.x86_64-linux.fw-fanctrl = (
+      import nixpkgs {
+        currentSystem = "x86_64-linux";
+        localSystem = "x86_64-linux";
+      }).pkgs.callPackage ./nix/packages/fw-fanctrl.nix {};
+
+    packages.x86_64-linux.fw-ectool = (
+      import nixpkgs {
+        currentSystem = "x86_64-linux";
+        localSystem = "x86_64-linux";
+      }).pkgs.callPackage ./nix/packages/fw-ectool.nix {};
+
+    nixosModules.default = import ./nix/module.nix;
+  };
+}
---
+++ nix/module.nix
@@ -0,0 +1,118 @@
+{ options, config, lib, pkgs, stdenv, ... }:
+
+with lib;
+with lib.types;
+let
+  cfg = config.programs.fw-fanctrl;
+  fw-ectool = pkgs.callPackage ./packages/fw-ectool.nix {};
+  fw-fanctrl = pkgs.callPackage ./packages/fw-fanctrl.nix {};
+  defaultConfig = builtins.fromJSON (builtins.readFile ../config.json);
+in
+{
+  options.programs.fw-fanctrl = {
+    enable = mkOption {
+      type = bool;
+      default = false;
+      description = ''
+        Enable fw-fanctrl systemd service and install the needed packages.
+      '';
+    };
+    config = {
+      defaultStrategy = mkOption {
+        type = str;
+        default = defaultConfig.defaultStrategy;
+        description = "Default strategy to use";
+      };
+      strategyOnDischarging = mkOption {
+        type = str;
+        default = defaultConfig.strategyOnDischarging; 
+        description = "Default strategy on discharging";
+      };
+      batteryChargingStatusPath = mkOption {
+        type = str;
+        default = "/sys/class/power_supply/BAT1/status";
+        description = "";
+      };
+      strategies = mkOption {
+        default = defaultConfig.strategies;
+        type = attrsOf (submodule (
+          { options, name, ... }:
+          {
+            options = {
+              name = mkOption {
+                type = str;
+                default = "";
+                description = "Name of the strategy";
+              };
+              fanSpeedUpdateFrequency = mkOption {
+                type = int;
+                default = 5;
+                description = "How often the fan speed should be updated in seconds";
+              };
+              movingAverageInterval = mkOption {
+                type = int;
+                default = 25;
+                description = "Interval (seconds) of the last temperatures to use to calculate the average temperature";
+              };
+              speedCurve = mkOption {
+                default = [];
+                description = "How should the speed curve look like";
+                type = listOf (submodule (
+                  { options, ... }:
+                  {
+                    options = {
+                      temp = mkOption {
+                        type = int;
+                        default = 0;
+                        description = "Temperature at which the fan speed should be changed";
+                      };
+                      speed = mkOption {
+                        type = int;
+                        default = 0;
+                        description = "Percent how fast the fan should run at";
+                      };
+                    };
+                  }
+                ));
+              }; 
+            };
+          }
+        ));
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Install package
+    environment.systemPackages = [
+      fw-fanctrl
+      fw-ectool
+    ];
+
+    # Create config
+    environment.etc."fw-fanctrl/config.json" = {
+      text = builtins.toJSON cfg.config;
+    };
+
+    # Create Service
+    systemd.services.fw-fanctrl = {
+      description = "Framework Fan Controller";
+      after = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        ExecStart = "${fw-fanctrl}/bin/fw-fanctrl --run --config /etc/fw-fanctrl/config.json --no-log";
+        ExecStopPost = "${fw-ectool}/bin/ectool autofanctrl";
+      };
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    # Create suspend config
+    environment.etc."systemd/system-sleep/fw-fanctrl-suspend.sh".source = pkgs.writeShellScript "fw-fanctrl-suspend" (
+      builtins.replaceStrings [ ''/usr/bin/python3 "%PREFIX_DIRECTORY%/bin/fw-fanctrl"'' "/bin/bash" ] [ "${fw-fanctrl}/bin/fw-fanctrl" "" ] (
+        builtins.readFile ../services/system-sleep/fw-fanctrl-suspend
+      )
+    );
+  };
+}
