ccd53427e0ea8953fe36b24fd7b78910aa605e3e
sai <oli@smail.is>
2024-08-18 13:59:14 -0500

    update the installer to use existing placeholder format

--- install.sh
+++ install.sh
@@ -145,11 +145,7 @@ function install() {
 
     # add --no-battery flag to the fanctrl service if specified
     if [ "$NO_BATTERY_SENSOR" = true ]; then
-        origLine=$(grep "ExecStart=/usr/bin/python3" ./services/fw-fanctrl.service)
-        if ! grep -q -- "--no-battery-sensors" <<< "$origLine"; then
-            newLine="${origLine} --no-battery-sensors"
-            sed -i "s#$origLine#$newLine#" ./services/fw-fanctrl.service
-        fi
+        NO_BATTERY_MODE_OPTION="--no-battery-sensors"
     fi
 
     # create program services based on the services present in the './services' folder
@@ -163,7 +159,7 @@ function install() {
             systemctl stop "$SERVICE"
         fi
         echo "creating '$DEST_DIR$PREFIX_DIR/lib/systemd/system/$SERVICE$SERVICE_EXTENSION'"
-        cat "$SERVICES_DIR/$SERVICE$SERVICE_EXTENSION" | sed -e "s/%PREFIX_DIRECTORY%/${PREFIX_DIR//\//\\/}/" | sed -e "s/%SYSCONF_DIRECTORY%/${SYSCONF_DIR//\//\\/}/" | tee "$DEST_DIR$PREFIX_DIR/lib/systemd/system/$SERVICE$SERVICE_EXTENSION" > "/dev/null"
+        cat "$SERVICES_DIR/$SERVICE$SERVICE_EXTENSION" | sed -e "s/%PREFIX_DIRECTORY%/${PREFIX_DIR//\//\\/}/" | sed -e "s/%SYSCONF_DIRECTORY%/${SYSCONF_DIR//\//\\/}/" | sed -e "s/%NO_BATTERY_MODE_OPTION%/${NO_BATTERY_MODE_OPTION}/" | tee "$DEST_DIR$PREFIX_DIR/lib/systemd/system/$SERVICE$SERVICE_EXTENSION" > "/dev/null"
     done
 
     # add program services sub-configurations based on the sub-configurations present in the './services' folder
