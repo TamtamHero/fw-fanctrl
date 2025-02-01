ab36905b418aa5ebb25df66dcffeeb43d7716b72
Svenum <s.ziegler@holypenguin.net>
2025-02-01 20:47:25 +0100

    fix missing option

--- nix/module.nix
+++ nix/module.nix
@@ -107,7 +107,7 @@ in
       serviceConfig = {
         Type = "simple";
         Restart = "always";
-        ExecStart= "${fw-fanctrl}/bin/fw-fanctrl --output-format \"JSON\" run --config \"/etc/fw-fanctrl/config.json\" --silent ${lib.strings.optionalString cfg.disableBatteryTempCheck}";
+        ExecStart= "${fw-fanctrl}/bin/fw-fanctrl --output-format \"JSON\" run --config \"/etc/fw-fanctrl/config.json\" --silent ${lib.strings.optionalString cfg.disableBatteryTempCheck "--no-battery-sensors"}";
         ExecStopPost = "${pkgs.fw-ectool}/bin/ectool autofanctrl";
       };
       enable = true;
